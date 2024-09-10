d0e48d7563f2682121d707831ab401e77be8e298
Melle Klein Goldewijk <M.KleinGoldewijk@tilburguniversity.edu>
2024-09-10 16:27:40 +0200

    download_data.R

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,6 +1,8 @@
-# download dataset from github
+# create directory to save project
 dir.create("./dprep-project-team-14")
 dir.create("./dprep-project-team-14/data")
+
+# download data
 download.file("https://raw.githubusercontent.com/course-dprep/project-team-14/main/data/sample_dataset_dprep.csv", "./dprep-project-team-14/data/dataset_dprep.csv")
 
 
