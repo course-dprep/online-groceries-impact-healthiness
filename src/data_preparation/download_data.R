964f282e12447a57789af4da56b0c6f9d805d0db
Melle Klein Goldewijk <M.KleinGoldewijk@tilburguniversity.edu>
2024-09-19 17:02:31 +0200

    update data exploration

--- src/data_preparation/download_data.R
+++ src/data_preparation/download_data.R
@@ -1,8 +1,4 @@
-# create directory to save project
-dir.create("./dprep-project-team-14")
-dir.create("./dprep-project-team-14/data")
-
 # download data
-download.file("https://raw.githubusercontent.com/course-dprep/project-team-14/main/data/sample_dataset_dprep.csv", "./dprep-project-team-14/data/dataset_dprep.csv")
+download.file("https://raw.githubusercontent.com/course-dprep/online-groceries-impact-healthiness/refs/heads/main/data/dataset_dprep.csv", "./dprep-project-team-14/data/dataset_dprep.csv")
 
 
