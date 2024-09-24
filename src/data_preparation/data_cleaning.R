3269b6e9e70a9827708d02ff0404ab5e3bb9bdf9
Melle Klein Goldewijk <M.KleinGoldewijk@tilburguniversity.edu>
2024-09-24 16:20:08 +0200

    make update

--- src/data_preparation/data_cleaning.R
+++ src/data_preparation/data_cleaning.R
@@ -2,6 +2,8 @@
 library(data.table)
 library(dplyr)
 
+# create directory to store output
+dir.create("../../gen/data_preparation/input", recursive = TRUE)
 
 DT <- fread("../../data/dataset_dprep.csv")
 
@@ -57,4 +59,4 @@ weekly_baskets <- setDT(DT %>% group_by(panelist, week_number) %>%
                     prop_expenditure_vegetables = sum(value[segment == "groente"]) / sum(value) * 100) %>%
                   ungroup() )
 
-fwrite(weekly_baskets, "../../gen/data_preparation/weekly_baskets.csv")                    
+fwrite(weekly_baskets, "../../gen/data_preparation/input/weekly_baskets.csv")                    
--- src/data_preparation/download_data.R
+++ src/data_preparation/download_data.R
@@ -1,3 +1,5 @@
+# create directory to store data
+dir.create("../../data")
 
 # download and save data
 download.file("https://raw.githubusercontent.com/course-dprep/online-groceries-impact-healthiness/refs/heads/main/data/dataset_dprep.csv", "../../data/dataset_dprep.csv")
