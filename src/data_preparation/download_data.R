07e82003815e627226a61a7aa92ae15db2c1666c
Melle Klein Goldewijk <M.KleinGoldewijk@tilburguniversity.edu>
2024-09-24 15:11:12 +0200

    undo remove

---
+++ src/data_preparation/download_data.R
@@ -0,0 +1,4 @@
+# download and save data
+download.file("https://raw.githubusercontent.com/course-dprep/online-groceries-impact-healthiness/refs/heads/main/data/dataset_dprep.csv", "../../data/dataset_dprep.csv")
+
+
