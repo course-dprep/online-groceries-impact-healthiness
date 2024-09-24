99c4bb742ae09793e3a1f3f4e923ac8f9e32eb86
Melle Klein Goldewijk <M.KleinGoldewijk@tilburguniversity.edu>
2024-09-24 16:51:35 +0200

    exploration

--- src/data_preparation/install_packages.R
+++ src/data_preparation/install_packages.R
@@ -1,12 +1,4 @@
-# create sub-directories
-dir.create("../..data")
-dir.create("../..gen")
-dir.create("../..gen/data-preparation")
-dir.create("../..gen/analysis")
-dir.create("../..gen/paper")
-
-
-# installing and loading required R packages
+# installing required R packages
 
 required_packages <- c("data.table", "dplyr", "xtable", "tinytex", "ggplot2")
 
@@ -14,10 +6,4 @@ required_packages <- c("data.table", "dplyr", "xtable", "tinytex", "ggplot2")
 installed_packages <- rownames(installed.packages())
 for (pkg in required_packages) {
   if (!pkg %in% installed_packages) 
-    {install.packages(pkg, dependencies = TRUE)}
-}
-
-# Load the packages
-lapply(required_packages, library, character.only = TRUE)
-
-rm(pkg, installed_packages, required_packages)
+    {install.packages(pkg, dependencies = TRUE)}}
