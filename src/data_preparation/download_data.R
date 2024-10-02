e426c2e14353c486d1964034642d70af906f71ad
Melle Klein Goldewijk <m.kleingoldewijk@tilburguniversity.edu>
2024-10-02 11:11:32 +0200

    Delete src/data_preparation directory

--- src/data_preparation/download_data.R
+++
@@ -1,7 +0,0 @@
-# create directory to store data
-dir.create("../../data")
-
-# download and save data
-download.file("https://raw.githubusercontent.com/course-dprep/online-groceries-impact-healthiness/refs/heads/main/data/dataset_dprep.csv", "../../data/dataset_dprep.csv")
-
-
--- src/data_preparation/install_packages.R
+++
@@ -1,9 +0,0 @@
-# installing required R packages
-
-required_packages <- c("data.table", "dplyr", "xtable", "tinytex", "ggplot2")
-
-# Install missing packages
-installed_packages <- rownames(installed.packages())
-for (pkg in required_packages) {
-  if (!pkg %in% installed_packages) 
-    {install.packages(pkg, dependencies = TRUE)}}
