f04f4783518ae2702d137b3845f9f935540274ff
Melle Klein Goldewijk <M.KleinGoldewijk@tilburguniversity.edu>
2024-10-02 11:38:07 +0200

    change data variable names in source code

--- src/data_preparation/weekly_aggregation.R
+++ src/data_preparation/weekly_aggregation.R
@@ -3,14 +3,14 @@ library(data.table)
 library(dplyr)
 
 # open data
-DT <- fread("../../gen/data_preparation/input/data_clean.csv")
+purchases_2019 <- fread("../../gen/data_preparation/input/data_clean.csv")
 
 # weekly basket aggregation
-weekly_baskets <- setDT(DT %>% group_by(panelist, week_number) %>%
+weekly_baskets_2019 <- setDT(purchases_2019 %>% group_by(panelist, week_number) %>%
                   summarise(
                     treatment_group = first(shopper_type),
                     cohort_period = first(first_online_week),
                     prop_expenditure_vegetables = sum(value[segment == c("groente", "fruit")]) / sum(value) * 100) %>%
                   ungroup() )
 
-fwrite(weekly_baskets, "../../gen/data_preparation/input/weekly_baskets.csv")    
+fwrite(weekly_baskets_2019, "../../gen/data_preparation/input/weekly_baskets_2019.csv")    
