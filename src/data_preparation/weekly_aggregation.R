53d110d19fd2ca58057060727725e294e2fdabc8
Melle Klein Goldewijk <M.KleinGoldewijk@tilburguniversity.edu>
2024-10-07 15:42:17 +0200

    make update DP fix

--- src/data_preparation/weekly_aggregation.R
+++ src/data_preparation/weekly_aggregation.R
@@ -10,7 +10,7 @@ weekly_baskets_2019 <- setDT(purchases_2019 %>% group_by(panelist, week_number) 
                   summarise(
                     treatment_group = first(shopper_type),
                     cohort_period = first(first_online_week),
-                    prop_expenditure_vegetables = sum(value[segment == c("groente", "fruit")]) / sum(value) * 100) %>%
+                    prop_expenditure_fruit_vegetables = sum(value[segment %in% c("groente", "fruit")]) / sum(value) * 100) %>%
                   ungroup() )
 
 fwrite(weekly_baskets_2019, "../../gen/data_preparation/input/weekly_baskets_2019.csv")    
